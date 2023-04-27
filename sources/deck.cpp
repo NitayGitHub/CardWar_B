16221fc374f01ec6cfda9ca8016318938330c9cc
nitayGitHub <nitaylevi1998@gmail.com>
2023-04-27 16:32:04 +0300

    deckiniupd

--- sources/deck.cpp
+++ sources/deck.cpp
@@ -19,62 +19,26 @@ const int ten = 10;
 const int jack = 11;
 const int queen = 12;
 const int king = 13;
+const string types[4] = {"Spades", "Hearts", "Clubs", "Diamonds"};
 
 const int deckSize = 52;
 Deck::Deck()
 {
-    deck.push_back(Card(ace, "Ace of Spades"));
-    deck.push_back(Card(two, "Two of Spades"));
-    deck.push_back(Card(three, "Three of Spades"));
-    deck.push_back(Card(four, "Four of Spades"));
-    deck.push_back(Card(five, "Five of Spades"));
-    deck.push_back(Card(six, "Six of Spades"));
-    deck.push_back(Card(seven, "Seven of Spades"));
-    deck.push_back(Card(eight, "Eight of Spades"));
-    deck.push_back(Card(nine, "Nine of Spades"));
-    deck.push_back(Card(ten, "Ten of Spades"));
-    deck.push_back(Card(jack, "Jack of Spades"));
-    deck.push_back(Card(queen, "Queen of Spades"));
-    deck.push_back(Card(king, "King of Spades"));
-    deck.push_back(Card(ace, "Ace of Hearts"));
-    deck.push_back(Card(two, "Two of Hearts"));
-    deck.push_back(Card(three, "Three of Hearts"));
-    deck.push_back(Card(four, "Four of Hearts"));
-    deck.push_back(Card(five, "Five of Hearts"));
-    deck.push_back(Card(six, "Six of Hearts"));
-    deck.push_back(Card(seven, "Seven of Hearts"));
-    deck.push_back(Card(eight, "Eight of Hearts"));
-    deck.push_back(Card(nine, "Nine of Hearts"));
-    deck.push_back(Card(ten, "Ten of Hearts"));
-    deck.push_back(Card(jack, "Jack of Hearts"));
-    deck.push_back(Card(queen, "Queen of Hearts"));
-    deck.push_back(Card(king, "King of Hearts"));
-    deck.push_back(Card(ace, "Ace of Clubs"));
-    deck.push_back(Card(two, "Two of Clubs"));
-    deck.push_back(Card(three, "Three of Clubs"));
-    deck.push_back(Card(four, "Four of Clubs"));
-    deck.push_back(Card(five, "Five of Clubs"));
-    deck.push_back(Card(six, "Six of Clubs"));
-    deck.push_back(Card(seven, "Seven of Clubs"));
-    deck.push_back(Card(eight, "Eight of Clubs"));
-    deck.push_back(Card(nine, "Nine of Clubs"));
-    deck.push_back(Card(ten, "Ten of Clubs"));
-    deck.push_back(Card(jack, "Jack of Clubs"));
-    deck.push_back(Card(queen, "Queen of Clubs"));
-    deck.push_back(Card(king, "King of Clubs"));
-    deck.push_back(Card(ace, "Ace of Diamonds"));
-    deck.push_back(Card(two, "Two of Diamonds"));
-    deck.push_back(Card(three, "Three of Diamonds"));
-    deck.push_back(Card(four, "Four of Diamonds"));
-    deck.push_back(Card(five, "Five of Diamonds"));
-    deck.push_back(Card(six, "Six of Diamonds"));
-    deck.push_back(Card(seven, "Seven of Diamonds"));
-    deck.push_back(Card(eight, "Eight of Diamonds"));
-    deck.push_back(Card(nine, "Nine of Diamonds"));
-    deck.push_back(Card(ten, "Ten of Diamonds"));
-    deck.push_back(Card(jack, "Jack of Diamonds"));
-    deck.push_back(Card(queen, "Queen of Diamonds"));
-    deck.push_back(Card(king, "King of Diamonds"));
+    for(int i = 0; i < 4; i++){
+        deck.push_back(Card(ace, "Ace of " + types[i]));
+        deck.push_back(Card(two, "Two of " + types[i]));
+        deck.push_back(Card(three, "Three of " + types[i]));
+        deck.push_back(Card(four, "Four of " + types[i]));
+        deck.push_back(Card(five, "Five of " + types[i]));
+        deck.push_back(Card(six, "Six of " + types[i]));
+        deck.push_back(Card(seven, "Seven of " + types[i]));
+        deck.push_back(Card(eight, "Eight of " + types[i]));
+        deck.push_back(Card(nine, "Nine of " + types[i]));
+        deck.push_back(Card(ten, "Ten of " + types[i]));
+        deck.push_back(Card(jack, "Jack of " + types[i]));
+        deck.push_back(Card(queen, "Queen of " + types[i]));
+        deck.push_back(Card(king, "King of " + types[i]));
+    }
 
 }
 
